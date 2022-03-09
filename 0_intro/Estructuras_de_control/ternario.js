e7c2cd9632e5e19480a466a6e337aac5baa08948
Jorge-Salas7 <jorge.salas@lasallistas.org.mx>
2022-03-08 20:43:19 -0600

    Estructuras de control

---
+++ 0_intro/Estructuras_de_control/ternario.js
@@ -0,0 +1,10 @@
+let edad=20;
+let esMayorDeEdad;
+
+edad >=18? esMayorDeEdad=true : esMayorDeEdad=false;
+
+alert(`Es mayor de edad: ${esMayorDeEdad? 'SI' : 'NO'}`);
+
+
+//Esta es otra opción más resumida
+/* edad>=18? alert('eres mayor de edad') : alert('Eres menor de edad'); */
