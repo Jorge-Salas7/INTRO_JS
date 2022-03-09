a3ece2b011d39c9629b8e07b86e485eff8583ff5
Jorge-Salas7 <jorge.salas@lasallistas.org.mx>
2022-03-08 21:30:02 -0600

    Estructuras de control

---
+++ 0_intro/Estructuras_de_control/ejercicio_switch.js
@@ -0,0 +1,21 @@
+let nombreDelAnimal= prompt("Nombre del animal: ");
+
+switch(nombreDelAnimal) {
+    case "tigre":
+    case "leon":
+    case "pantera":
+        alert('ggggrrrrr');
+        break;
+    case "lobo":
+        alert('aaauuuu');
+        break;
+    case "perro":
+        alert('guau guau...');
+        break;
+    case "pato":
+        alert('cuak cuak');
+        break;
+    default:
+        alert('No se sabe')
+        break;
+}
