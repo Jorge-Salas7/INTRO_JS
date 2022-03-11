7f47fed0e9222b6f255435962b0f215b8770da6b
Jorge-Salas7 <jorge.salas@lasallistas.org.mx>
2022-03-10 19:54:27 -0600

    Estructuras de control

---
+++ 0_intro/Estructuras_de_control/arreglos.js
@@ -0,0 +1,14 @@
+const arr1=new Array(3);
+const arr2=new Array(1, 2, 3, 4);
+const arr3=[1, 2, 3, 4];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+
+/*Arreglo mixto*/
+const arrMixto=["Jorge", 20, true, () => console.log("Hola"), "Salas"];
+console.log(arrMixto);
+
+arrMixto.length; /*Saber el tamaño del arreglo*/
---
+++ 0_intro/Estructuras_de_control/objetos.js
@@ -0,0 +1,9 @@
+const Jorge = {
+    mesCumple:"Octubre",
+    colorFavorito:"Verde",
+    pasatiempo:"Futbol",
+    mascota:{
+        nombreMascota:"Betta",
+        razaMascota:"Chihuahua"
+    }
+}
